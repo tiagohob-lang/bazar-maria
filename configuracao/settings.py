09a01240f953940bacd6731b3d7e4778f2d1fb2c
tiagohob-lang <tiagohob@gmail.com>
2026-04-14 22:01:13 -0300

    v12

--- configuracao/settings.py
+++ configuracao/settings.py
@@ -131,7 +131,8 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        #"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.StaticFilesStorage",
     },
 }
 
