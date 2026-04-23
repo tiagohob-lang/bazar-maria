b20e0fdc61d4cb8aa094ed12fce0ddbbe642a640
tiagohob-lang <tiagohob@gmail.com>
2026-04-23 15:21:15 -0300

    Versão 1.0 - v4

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Detecta mudanças novas
-python manage.py makemigrations --no-input
+python manage.py makemigrations core --no-input
 
 # Tenta aplicar ignorando conflitos de tabelas já existentes
 python manage.py migrate --fake-initial --no-input
