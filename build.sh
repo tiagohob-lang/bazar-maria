9f9d338ad7a2117f3969a23f1f4735ab02c58f22
tiagohob-lang <tiagohob@gmail.com>
2026-04-23 15:40:19 -0300

    Reiniciando banco de dados com estrutura completa

--- build.sh
+++ build.sh
@@ -3,6 +3,4 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-
-# O comando migrate vai usar o novo 0001_initial.py
-python manage.py migrate --fake-initial --no-input
+python manage.py migrate --no-input
